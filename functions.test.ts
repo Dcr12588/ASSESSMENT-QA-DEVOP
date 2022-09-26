f356008ba8a86893a28ed51c6a1e3ec0049c4dab
Danny Ramirez <dcr1999jr@gmail.com>
2022-09-26 01:59:46 -0700

    test

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,14 @@
 const {shuffleArray} = require('./utils')
 
-describe('shuffleArray should', () => {
-    // CODE HERE
+describe('shuffleArray', () => {
+
+    // test('Shuffle aray should return an array', () => {
+    //     expect(shuffleArray([1, 2, 3, 4, 5])).toEqual([1
+    // })
+
+    test('Shuffle aray should return an array of same length as argument', () => {
+        let arr = [1,2,3,4,5]
+        let result = shuffleArray(arr)
+        expect(arr.length).toBe(result.length)
+    })
 })
