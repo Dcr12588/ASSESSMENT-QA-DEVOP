fc99430fa7867b74e0896b135b86884d743e597a
Danny Ramirez <dcr1999jr@gmail.com>
2022-09-26 02:24:53 -0700

    tests added

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,17 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test('draw button displays character choices', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(500)
+    const myChoices = await driver.findElement(By.id('choices'))
+    expect(myChoices).not.toBe('')
+})
+
+test('add to duo button diplays characters added', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(500)
+    const myDuo = await driver.findElement(By.id('player-duo'))
+    expect(myDuo).not.toBe('')
+})
--- functions.test.ts
+++ functions.test.ts
@@ -2,9 +2,11 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray', () => {
 
-    // test('Shuffle aray should return an array', () => {
-    //     expect(shuffleArray([1, 2, 3, 4, 5])).toEqual([1
-    // })
+    test('Shuffle array should return an array', () => {
+        let arr = [1,2,3,4,5]
+        let result = shuffleArray(arr)
+        expect(arr).toEqual([1,2,3,4,5])
+    })
 
     test('Shuffle aray should return an array of same length as argument', () => {
         let arr = [1,2,3,4,5]
